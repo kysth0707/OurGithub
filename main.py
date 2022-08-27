510630c866b3e62fb66ec457513c7b6d768a8b8c
kysth0707 <kysth0707@naver.com>
2022-08-27 23:42:51 +0900

    Github api Request

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("안녕")
+import requests
+import json
+
+# GithubUser = "kysth0707"
+GithubUser = "banksemi"
+
+# https://api.github.com/users/kysth0707
+TestLink = f"https://api.github.com/users/{GithubUser}/repos"
+ResponseData = requests.get(TestLink)
+
+ResponseJsonData = json.loads(ResponseData.text)
+
+for i in range(len(ResponseJsonData)):
+	print(f"{ResponseJsonData[i]['name']} / fork : {ResponseJsonData[i]['fork']}")
