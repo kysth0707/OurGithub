7d2e6fc84508c0839e179201b7b05e682ac704e0
kysth0707 <kysth0707@naver.com>
2022-08-28 22:13:23 +0900

    사용자 이미지 가져오기

--- RequestFunctions.py
+++ RequestFunctions.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from io import BytesIO
+from PIL import Image
 
 TokenAPI = ""
 
@@ -12,6 +14,18 @@ def RequestGet(url):
 		return None
 
 	return json.loads(ResponseData.text)
+
+def RequestImageGet(url, SaveLoc):
+	Header = {'Authorization' : f'token {TokenAPI}'}
+
+
+	ResponseData = requests.get(url, headers=Header)
+	if not ResponseData.ok:
+		return None
+
+	a = Image.open(BytesIO(ResponseData.content))
+	a.save(SaveLoc)
+	return True
 
 def SetTokenAPI(Token):
 	global TokenAPI
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from RequestFunctions import RequestGet, SetTokenAPI
+from RequestFunctions import RequestGet, SetTokenAPI, RequestImageGet
+import os
+
+def ReturnPos(loc : str):
+	return os.getcwd() + loc
 
 GithubAPIToken = ""
 with open("E:\\GithubProjects\\githubapitoken.txt", "r", encoding="utf-8") as f:
@@ -8,24 +12,27 @@ with open("E:\\GithubProjects\\githubapitoken.txt", "r", encoding="utf-8") as f:
 GithubUser = "kysth0707"
 # GithubUser = "banksemi"
 
-GithubRepos = RequestGet(f"https://api.github.com/users/{GithubUser}/repos")
 
-for i in range(len(GithubRepos)):
-	RepoName = GithubRepos[i]['name']
-	IsFork = GithubRepos[i]['fork']
 
-	Commits = RequestGet(f"https://api.github.com/repos/{GithubUser}/{RepoName}/commits")
-	try:
-		LastCommit = Commits[0]['commit']['author']['date']
-	except:
-		LastCommit = "?"
+# GithubRepos = RequestGet(f"https://api.github.com/users/{GithubUser}/repos")
 
-	print(f"{RepoName} / fork : {IsFork} / LastCommit = {LastCommit}")
+# for i in range(len(GithubRepos)):
+# 	RepoName = GithubRepos[i]['name']
+# 	IsFork = GithubRepos[i]['fork']
+
+# 	Commits = RequestGet(f"https://api.github.com/repos/{GithubUser}/{RepoName}/commits")
+# 	try:
+# 		LastCommit = Commits[0]['commit']['author']['date']
+# 	except:
+# 		LastCommit = "?"
+
+# 	print(f"{RepoName} / fork : {IsFork} / LastCommit = {LastCommit}")
 
 print("\n")
 UserData = RequestGet(f"https://api.github.com/users/{GithubUser}")
 print(f"ID : {UserData['login']}")
 print(f"생성 날짜 : {UserData['created_at']}")
 print(f"리포지토리 개수 : {UserData['public_repos']}")
+RequestImageGet(UserData['avatar_url'], ReturnPos(f"\\imgs\\profiles\\{GithubUser}.png"))
 # avatar_url
 # followers_url
