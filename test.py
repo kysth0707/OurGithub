e5239f354cef92156c575166b0ec4b72e7a05d51
kysth0707 <kysth0707@naver.com>
2022-09-08 22:45:21 +0900

    branch test

--- test.py
+++ test.py
@@ -10,3 +10,5 @@ GithubUser = "kysth0707"
 Commits = RequestGet(f"https://api.github.com/repos/{GithubUser}/41x41display/commits")
 
 print(Commits[0]['commit']['author']['date']) # 가장 최근꺼임
+
+# Branch test
