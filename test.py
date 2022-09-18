878ef914ca6bfb7c79f1ce039a913333da0acd50
kysth0707 <kysth0707@naver.com>
2022-09-18 16:39:21 +0900

    feat : Testing auto update.
    I should make it auto changing.
    Only the function of auto download is avilable now.

--- test.py
+++ test.py
@@ -1,14 +1,49 @@
-from ModuleRequestFunc import RequestGet, SetTokenAPI
+# from ModuleRequestFunc import RequestGet, SetTokenAPI
 
-GithubAPIToken = ""
-with open("E:\\GithubProjects\\githubapitoken.txt", "r", encoding="utf-8") as f:
-	GithubAPIToken = f.readline()
-	SetTokenAPI(GithubAPIToken)
+# GithubAPIToken = ""
+# with open("E:\\GithubProjects\\githubapitoken.txt", "r", encoding="utf-8") as f:
+# 	GithubAPIToken = f.readline()
+# 	SetTokenAPI(GithubAPIToken)
 
-GithubUser = "kysth0707"
+# GithubUser = "kysth0707"
 
-Commits = RequestGet(f"https://api.github.com/repos/{GithubUser}/41x41display/commits")
+# Commits = RequestGet(f"https://api.github.com/repos/{GithubUser}/41x41display/commits")
 
-print(Commits[0]['commit']['author']['date']) # 가장 최근꺼임
+# print(Commits[0]['commit']['author']['date']) # 가장 최근꺼임
 
-# Branch test
+# # Branch test
+
+
+# Auto Download Test
+
+import requests
+import os
+import zipfile
+
+def ReturnPos(loc : str):
+	return os.getcwd() + loc
+
+url = "https://github.com/kysth0707/OurGithub/releases/download/v1.0/OurGithub_v1.0.zip"
+DownloadLoc = '\\AutoUpdate'
+
+
+response = requests.get(url)
+
+try:
+	os.remove(ReturnPos(DownloadLoc))
+except:
+	pass
+
+open(ReturnPos(DownloadLoc+"\\Download\\temp.zip"), "wb").write(response.content)
+
+MyZip = zipfile.ZipFile(ReturnPos(DownloadLoc+"\\Download\\temp.zip"))
+MyZip.extractall(ReturnPos(DownloadLoc+"\\Download"))
+
+Files = os.listdir(ReturnPos(DownloadLoc+"\\Download"))
+FileName = ""
+for Name in Files:
+	if Name != "temp.zip":
+		FileName = Name
+
+MyZip = zipfile.ZipFile(ReturnPos(DownloadLoc+"\\Download\\"+FileName))
+MyZip.extractall(ReturnPos(DownloadLoc+"\\ZipExtract"))
